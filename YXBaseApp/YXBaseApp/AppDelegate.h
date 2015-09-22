fad0865313e840beaf3def70e3b13bb3df365e4e
Yunxin.Li <liyunxin2006@163.com>
2015-09-22 18:38:43 +0800

    add project file

---
+++ YXBaseApp/YXBaseApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXBaseApp
+//
+//  Created by Yunxin.Li on 15/9/22.
+//  Copyright © 2015年 CJW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXBaseApp/YXBaseApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YXBaseApp
+//
+//  Created by Yunxin.Li on 15/9/22.
+//  Copyright © 2015年 CJW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
