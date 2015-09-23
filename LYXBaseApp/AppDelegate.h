511f82a5403aa836fa91b7970de20d6fa408b78f
Yunxin.Li <liyunxin2006@163.com>
2015-09-23 11:26:28 +0800

    add project files

---
+++ LYXBaseApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYXBaseApp
+//
+//  Created by Yunxin.Li on 15/9/23.
+//  Copyright © 2015年 LYX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
