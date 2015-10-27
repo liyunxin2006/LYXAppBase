1fd95ede1f8b87958b47447dde597c7c2625aad4
Yunxin.Li <liyunxin2006@163.com>
2015-10-27 16:09:59 +0800

    Add files

---
+++ LYXBaseApp/View/UISearchBar+LYXShowsCancelButton.h
@@ -0,0 +1,13 @@
+//
+//  UISearchBar+LYXShowsCancelButton.h
+//  LYXBaseApp
+//
+//  Created by Yunxin.Li on 15/10/27.
+//  Copyright © 2015年 LYX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UISearchBar (LYXShowsCancelButton)
+
+@end
